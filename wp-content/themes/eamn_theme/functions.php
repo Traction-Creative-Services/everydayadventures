dff1e397f086e50cd420fe4e4124f8a5bf9e62f9
Martin M Sheeks <martin.sheeks@gmail.com>
2014-07-25 09:12:41 -0500

    background change
    change the login background

--- wp-content/themes/eamn_theme/functions.php
+++ wp-content/themes/eamn_theme/functions.php
@@ -27,6 +27,7 @@
 	    <style type="text/css">
 	        body.login div#login h1 a {
 	            background-image: url("<?php echo get_stylesheet_directory_uri(); ?>/resources/images/mn.png");
+	            background-repeat:repeat;
 	            padding-bottom: 30px;
 	        }
 
